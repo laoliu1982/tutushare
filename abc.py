d5c5d7ece534e70d3ddc96a7b2c79d0c71b61297
Lewei50 <service@lewei50.com>
2017-07-17 17:57:55 +0800

    go on

--- abc.py
+++ abc.py
@@ -4,5 +4,16 @@ import time
 print (time.time())
 #df=ts.get_sina_dd('601318',date="2017-07-14",vol=3000)
 df=ts.get_hist_data('601318','2017-07-01')
+#df=ts.get_sina_dd('601318','2017-07-17',vol=2000)
 print (df)
-print (type(df))
+print (df.dtypes)
+print ('index \r\n')
+print (df.index)
+print ('column \r\n')
+print (df.columns)
+print (df.values)
+print ("test select")
+#do=df.iloc[:,4]
+do=df.loc[:,['close','volume']]
+print (do)
+

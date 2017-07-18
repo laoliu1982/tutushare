2370939ab436218d8955a5d063f71769c65d3981
laoliu1982 <laoliu1982@gmail.com>
2017-07-19 07:43:03 +0800

    put in out vol in pandas dateframe

--- abc.py
+++ abc.py
@@ -5,26 +5,13 @@ import time
 import pandas as pd
 import numpy as np
 from numpy import array
-
+import matplotlib.pyplot as plt
 from datetime import datetime
 print (time.strftime("%Y-%m-%d %H:%M:%S"))
 #df=ts.get_sina_dd('601318',date="2017-07-18",vol=3000)
-'''
-df=ts.get_hist_data('601318','2017-07-01')
-#df=ts.get_sina_dd('601318','2017-07-17',vol=2000)
-print (df)
-print (df.dtypes)
-print ('index \r\n')
-print (df.index)
-print ('column \r\n')
-print (df.columns)
-print (df.values)
-print ("test select")
+iovol=np.zeros((12,6))
+print (iovol)
 
-#do=df.iloc[:,4]
-do=df.loc[:,['close','volume','price_change','p_change','turnover']]
-print (do)
-'''
 def datelist(beginDate, endDate):
     # beginDate, endDate是形如‘20160601’的字符串或datetime格式
     date_l=[datetime.strftime(x,'%Y-%m-%d') for x in list(pd.date_range(start=beginDate, end=endDate))]
@@ -32,39 +19,40 @@ def datelist(beginDate, endDate):
 
 #dates = pd.date_range('20170701',periods =6)
 #print(dates)
-dayrange = datelist("2017-07-03", "2017-07-5")
+dayrange = datelist("2017-07-03", "2017-07-18")
 print (dayrange)
-print (type(dayrange))
-#print (list('abcd'))
-df = pd.DataFrame(np.random.randn(3,2), index=dayrange, columns=['in','out'])
-print (df)
-print (type(np.random.randn(3,2)))
-print (np.random.randn(3,2))
+
 print ("--------------------------------------------")
 dayinstock=[]
-volin=[]
-volout=[]
 
-test =[[]]
-
+row=0
 for x in dayrange:
-
-    df=ts.get_sina_dd('601318',x,vol=2000)
+    df=ts.get_sina_dd('601318',x,vol=400)
     if not df is None:
+
         print (x)
         dayinstock.append(x)
-        volin.append(df[df.type=='买盘']['volume'].sum())
-        volout.append(df[df.type == '卖盘']['volume'].sum())
-        time.sleep(2)
+        iovol[row][0]=df[df.type=='买盘']['volume'].sum()
 
+        iovol[row][1]=df[df.type == '卖盘']['volume'].sum()
+        time.sleep(0.1)
+        df=ts.get_sina_dd('601318',x,vol=1000)
+        iovol[row][2]=df[df.type=='买盘']['volume'].sum()
+        iovol[row][3]=df[df.type == '卖盘']['volume'].sum()
+        time.sleep(0.1)
+        df=ts.get_sina_dd('601318',x,vol=2000)
+        iovol[row][4]=df[df.type=='买盘']['volume'].sum()
+        iovol[row][5]=df[df.type == '卖盘']['volume'].sum()
+        row=row+1
+print (iovol)
 
-print(volin)
-print (test)
-print(type(test))
-print(np.array(volin))
-test.append(volin)
-test.append(volout)
-print(np.array(test))
+dates=pd.DataFrame(iovol,index=dayinstock,columns=['in400','out40','in1000','out1000','in2000','out2000'])
+print (dates)
+dates.to_csv('abc.csv')
+dates=dates.cumsum()
+plt.figure()
+dates.plot()
+plt.legend(loc= 'best')
 
 
 '''

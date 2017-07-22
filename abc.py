0156a394a79f2a50b7757a07af26f10448538bed
laoliu1982 <laoliu1982@gmail.com>
2017-07-22 23:10:43 +0800

    hello

--- abc.py
+++ abc.py
@@ -8,22 +8,27 @@ from numpy import array
 import matplotlib.pyplot as plt
 from datetime import datetime
 print (time.strftime("%Y-%m-%d %H:%M:%S"))
+stock ='601318'
 #df=ts.get_sina_dd('601318',date="2017-07-18",vol=3000)
-iovol=np.zeros((15,10))
-print (iovol)
+iovol=np.zeros((15,11))
+#iovol=np.arange(1500).reshape(150,10)
 
-stock = '601318'
+#print (iovol)
+
 def datelist(beginDate, endDate):
     # beginDate, endDate是形如‘20160601’的字符串或datetime格式
     date_l=[datetime.strftime(x,'%Y-%m-%d') for x in list(pd.date_range(start=beginDate, end=endDate))]
     return date_l
 
-dayrange = datelist("2017-07-03", "2017-07-21")
+dayrange = datelist("2017-06-03", "2017-07-21")
 print (dayrange)
 print ("--------------------------------------------")
 dayinstock=[]
+print (ts.get_sina_dd(stock,date='2017-06-28'))
 row=0
+inoutspan=0
 for x in dayrange:
+
     df=ts.get_sina_dd(stock,x,vol=400)
     if not df is None:
         
@@ -41,6 +46,10 @@ for x in dayrange:
         iovol[row][0]=df[df.type=='买盘']['volume'].sum()
 
         iovol[row][1]=df[df.type == '卖盘']['volume'].sum()
+        iovol[row][10]=inoutspan+iovol[row][0]-iovol[row][1]
+        inoutspan=iovol[row][10]
+
+
         time.sleep(0.1)
         df=ts.get_sina_dd(stock,x,vol=1000)
         iovol[row][2]=df[df.type=='买盘']['volume'].sum()
@@ -50,9 +59,13 @@ for x in dayrange:
         iovol[row][4]=df[df.type=='买盘']['volume'].sum()
         iovol[row][5]=df[df.type == '卖盘']['volume'].sum()
         row=row+1
-print (iovol)
+finalArray=iovol[0:row,:]
+print (finalArray)
 
-dates=pd.DataFrame(iovol,index=dayinstock,columns=['in400','out40','in1000','out1000','in2000','out2000','volume','p_change','turnover','close'])
+#dates=pd.DataFrame(finalArray,index=dayinstock,columns=['in400','out40','in1000','out1000','in2000','out2000','volume','p_change','turnover','close'])
+
+dates=pd.DataFrame(iovol,index=dayinstock,columns=['in400','out40','in1000','out1000','in2000','out2000','volume','p_change','turnover','close','inoutspan'])
+
 print (dates)
 csvname=stock+'.csv'
 dates.to_csv(csvname)
--- tmp.py
+++ tmp.py
@@ -1,4 +1,6 @@
 import tushare as ts
-df = ts.get_hist_data('601318',start='2017-07-01',end='2017-07-20')
-ds = df.loc[:,['close','volume','p_change','price_change','turnover']]
+#df = ts.get_hist_data('601318',start='2017-06-01',end='2017-07-20')
+#ds = df.loc[:,['close','volume','p_change','price_change','turnover']]
+ds = ts.get_sina_dd('601318',date='2017-06-28',vol=100)
 print (ds)
+

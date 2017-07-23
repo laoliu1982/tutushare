2538b01ab9d9ff06aebee6abee57f3cd3ffb3ad1
laoliu1982 <laoliu1982@gmail.com>
2017-07-23 14:55:22 +0800

    go

--- abc.py
+++ abc.py
@@ -10,7 +10,6 @@ from datetime import datetime
 print (time.strftime("%Y-%m-%d %H:%M:%S"))
 stock ='601318'
 #df=ts.get_sina_dd('601318',date="2017-07-18",vol=3000)
-iovol=np.zeros((15,11))
 #iovol=np.arange(1500).reshape(150,10)
 
 #print (iovol)
@@ -25,49 +24,54 @@ print (dayrange)
 print ("--------------------------------------------")
 dayinstock=[]
 print (ts.get_sina_dd(stock,date='2017-06-28'))
-row=0
-inoutspan=0
-for x in dayrange:
 
-    df=ts.get_sina_dd(stock,x,vol=400)
-    if not df is None:
-        
-        dt=ts.get_hist_data(stock,start=x,end=x)
-        dtt=dt.loc[x,['volume','p_change','turnover','close']]
-        print (type(dtt))
-        print (dtt)
+iovol=np.zeros((50,11))
+
+def oneinmonth(days,stock):
+    row=0
+    inoutspan=0
+    dayinstock=[]
+    for x in dayrange:
         print (x)
-        iovol[row][6]=dtt.iloc[0]
-        iovol[row][7]=dtt.iloc[1]
-        iovol[row][8]=dtt.iloc[2]
-        iovol[row][9]=dtt.iloc[3]
+        df=ts.get_sina_dd(stock,x,vol=400)
+        if not df is None:
+            dt=ts.get_hist_data(stock,start=x,end=x)
+            dtt=dt.loc[x,['volume','p_change','turnover','close']]
+            print (type(dtt))
+            print (dtt)
+            print (x)
+            iovol[row][6]=dtt.iloc[0]
+            iovol[row][7]=dtt.iloc[1]
+            iovol[row][8]=dtt.iloc[2]
+            iovol[row][9]=dtt.iloc[3]
 
-        dayinstock.append(x)
-        iovol[row][0]=df[df.type=='买盘']['volume'].sum()
+            dayinstock.append(x)
+            iovol[row][0]=df[df.type=='买盘']['volume'].sum()
 
-        iovol[row][1]=df[df.type == '卖盘']['volume'].sum()
-        iovol[row][10]=inoutspan+iovol[row][0]-iovol[row][1]
-        inoutspan=iovol[row][10]
+            iovol[row][1]=df[df.type == '卖盘']['volume'].sum()
+            iovol[row][10]=inoutspan+iovol[row][0]-iovol[row][1]
+            inoutspan=iovol[row][10]
 
+            time.sleep(0.1)
+            df=ts.get_sina_dd(stock,x,vol=1000)
+            iovol[row][2]=df[df.type=='买盘']['volume'].sum()
+            iovol[row][3]=df[df.type == '卖盘']['volume'].sum()
+            time.sleep(0.1)
+            df=ts.get_sina_dd(stock,x,vol=2000)
+            iovol[row][4]=df[df.type=='买盘']['volume'].sum()
+            iovol[row][5]=df[df.type == '卖盘']['volume'].sum()
+            row=row+1
+    finalArray=iovol[0:row,:]
+    print (finalArray)
+    print (dayinstock)
+    print (iovol)
+    dates=pd.DataFrame(finalArray,index=dayinstock,columns=['in400','out40','in1000','out1000','in2000','out2000','volume','p_change','turnover','close','inoutspan'])
+   # dates=pd.DataFrame(iovol,index=dayinstock,columns=['in400','out40','in1000','out1000','in2000','out2000','volume','p_change','turnover','close','inoutspan'])
+    
+    print ('googluck')
+    print (dates)
+    csvname=stock+'.csv'
+    dates.to_csv(csvname)
 
-        time.sleep(0.1)
-        df=ts.get_sina_dd(stock,x,vol=1000)
-        iovol[row][2]=df[df.type=='买盘']['volume'].sum()
-        iovol[row][3]=df[df.type == '卖盘']['volume'].sum()
-        time.sleep(0.1)
-        df=ts.get_sina_dd(stock,x,vol=2000)
-        iovol[row][4]=df[df.type=='买盘']['volume'].sum()
-        iovol[row][5]=df[df.type == '卖盘']['volume'].sum()
-        row=row+1
-finalArray=iovol[0:row,:]
-print (finalArray)
-
-#dates=pd.DataFrame(finalArray,index=dayinstock,columns=['in400','out40','in1000','out1000','in2000','out2000','volume','p_change','turnover','close'])
-
-dates=pd.DataFrame(iovol,index=dayinstock,columns=['in400','out40','in1000','out1000','in2000','out2000','volume','p_change','turnover','close','inoutspan'])
-
-print (dates)
-csvname=stock+'.csv'
-dates.to_csv(csvname)
-
-
+oneinmonth(dayrange,'600036')
+oneinmonth(dayrange,'601318')
--- tmp.py
+++ tmp.py
@@ -1,6 +1,8 @@
 import tushare as ts
+import abc 
+def testfun(x,y,z):
+    print (x,y,z)
+
 #df = ts.get_hist_data('601318',start='2017-06-01',end='2017-07-20')
 #ds = df.loc[:,['close','volume','p_change','price_change','turnover']]
-ds = ts.get_sina_dd('601318',date='2017-06-28',vol=100)
-print (ds)
-
+print (abc)

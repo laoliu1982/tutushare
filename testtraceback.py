4898b926ffe3b1a67d088721b8c9d50ff8722411
laoliu1982 <laoliu1982@gmail.com>
2017-09-10 09:01:15 +0800

    traceback

--- testtraceback.py
+++ testtraceback.py
@@ -3,20 +3,27 @@ import tushare as ts
 import pandas as pd
 from datetime import datetime
 import time
+from sqlalchemy import create_engine
 def datelist(begin,end):
     date_l=[datetime.strftime(x,'%Y-%m-%d')for x in list(pd.date_range(start=begin,end=end))]
     return date_l
 
-    
-daterange_l=datelist('2017-09-01','2017-09-06')
+today=time.strftime('%Y-%m-%d',time.localtime())
+daterange_l=datelist('2017-06-01',today)
+engine=create_engine('mysql://user:password@xxx.com/tusharetest?charset=utf8')
 for x in daterange_l:
     try:
         print (x)
         df=ts.get_day_all(x)
-        df.to_csv('day'+x+'.csv')
-        time.sleep(0.2)
+        if not df is None:
+          df['date']=x
+          df.to_csv('day'+x+'.csv')
+          df.to_sql('test',engine,if_exists='append')
+          time.sleep(0.2)
+          print ('to csv succeed')
+          
     except Exception as e:
-        print (e)    
-        print (x+'is not workday')
+        print (x+'is not workday'+' error is'+str(e) )
         continue
+    
     

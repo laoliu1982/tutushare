2b61bf1a3e5cc81ba164fc4a35e2a21348cd0551
laoliu1982 <laoliu1982@gmail.com>
2017-09-11 07:20:21 +0800

    testtraceback

--- testtraceback.py
+++ testtraceback.py
@@ -1,3 +1,4 @@
+# -*- coding: UTF-8 -*-
 import traceback
 import tushare as ts
 import pandas as pd
@@ -9,8 +10,8 @@ def datelist(begin,end):
     return date_l
 
 today=time.strftime('%Y-%m-%d',time.localtime())
-daterange_l=datelist('2017-06-01',today)
-engine=create_engine('mysql://user:password@xxx.com/tusharetest?charset=utf8')
+daterange_l=datelist('2017-09-08',today)
+engine=create_engine('mysql://u:p@x.com/tusharetest?charset=utf8')
 for x in daterange_l:
     try:
         print (x)
@@ -18,7 +19,7 @@ for x in daterange_l:
         if not df is None:
           df['date']=x
           df.to_csv('day'+x+'.csv')
-          df.to_sql('test',engine,if_exists='append')
+          df.to_sql('utftest',engine,if_exists='append')
           time.sleep(0.2)
           print ('to csv succeed')
           

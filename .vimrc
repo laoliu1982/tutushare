10c29a67eac4a17eb524483356e18b6963f56d51
lewei50 <liushuo@lewei50.com>
2017-10-28 08:32:10 +0800

    gogogo

---
+++ .vimrc
@@ -0,0 +1,7 @@
+filetype indent plugin on
+set nu
+let g:pydiction_location='~/.vim/tools/pydiction/complete-dict'
+let g:pydiction_menu_height=3
+set tags =tags;
+map <F5> :exec 'w'<CR>:exec '!python %'<CR>
+
--- GetDayAll.py
+++ GetDayAll.py
@@ -13,25 +13,25 @@ def datelist(begin,end):
 def getConfig(ConfigFile):
     CurrentDir=os.getcwd()
     ParentDir=os.path.dirname(CurrentDir)
-    FilePath=os.path.join(ParentDir,ConfigFile)
-    with open(FilePath,'r') as f:
+   # FilePath=os.path.join(ParentDir,ConfigFile)
+    with open('/home/lewei50/config.txt','r') as f:
         x=f.read()
     return(x)
 
 
 today=time.strftime('%Y-%m-%d',time.localtime())
-daterange_l=datelist('2017-09-29',today)
+today='2017-10-20'
+daterange_l=datelist(today,today)
 engine=create_engine(getConfig('config.txt'))
 for x in daterange_l:
     try:
         df=ts.get_day_all(date=x)
         if not df is None:
             df['date']=x
-            print(df)
             df.to_sql('test',engine,if_exists='append',index=False)
             time.sleep(0.2)
     except Exception as e:
         print(str(e))
 
 
-    
+

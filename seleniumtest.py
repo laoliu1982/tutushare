29f3d5b587c535ead0033b69256a7ef25af38a51
laoliu1982 <laoliu1982@gmail.com>
2018-01-17 22:47:34 +0800

    add TLGX

--- seleniumtest.py
+++ seleniumtest.py
@@ -1,15 +1,25 @@
 from selenium import webdriver
 import time
-
-#browser = webdriver.Chrome()
-browser=webdriver.PhantomJS()
+from datetime import datetime
+import json
+import requests
+headers={'userkey':'4f17f9f7a7'}
+browser = webdriver.Chrome()
+url='http://www.lewei50.com/api/V1/gateway/UpdateSensors/01'
+#browser=webdriver.PhantomJS()
 
 #browser.set_page_load_timeout(30)
-
-browser.get('http://hotels.ctrip.com/hotel/429200.html?isFull=F#ctm_ref=hod_sr_lst_dl_n_1_3')
-print (browser.title)
-page_info = browser.find_element_by_css_selector('p.staring_price>span.price')
-print(page_info.text)
+while(1):
+    browser.get('http://hotels.ctrip.com/hotel/429200.html?isFull=F#ctm_ref=hod_sr_lst_dl_n_1_3')
+    print(time.strftime("%H-%M-%S",time.localtime(time.time())))
+#print (browser.title)
+    page_info = browser.find_element_by_css_selector('p.staring_price>span.price')
+    print(page_info.text)
+    print (type(page_info.text))
+    payload=[{'Name':'TLGX','Value':page_info.text}]
+    r=requests.post(url,json.dumps(payload),headers=headers)
+    print(r.text)
+    time.sleep(300)
 '''
 pages = int((page_info.text.split('，')[0]).split(' ')[1])
 for page in range(pages):
